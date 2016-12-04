f3985818e349323441c368feca6b4810d1aa22db
Lakshan Mahagamage <lakshan.mahagamage@gmail.com>
2016-12-04 18:57:53 +0530

    Shop Page Layout

---
+++ woocommerce/archive-product.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * The Template for displaying product archives, including the main shop page which is a post type archive.
+ *
+ * Override this template by copying it to yourtheme/woocommerce/archive-product.php
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+$zenwares_sidebar_product = zenwares_options() -> getCpanelValue('zenwares_sidebar_product');
+if($zenwares_sidebar_product == ""): $zenwares_sidebar_product = "left"; endif;
+
+?>
+<?php get_template_part('header'); ?>
+<div class="listings-title">
+	<div class="container">
+		<div class="wrap-title">
+			<h1><span><?php woocommerce_page_title(); ?></span></h1>
+			<div class="bread">
+					<?php
+
+						if (function_exists('zenwares_breadcrumb')){
+							zenwares_breadcrumb('<div class="breadcrumbs theme-clearfix"><div class="container">', '</div></div>');
+						}
+
+					?>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+<div class="row">
+<?php if ( zenwares_sidebar_check('left-product') && $zenwares_sidebar_product == 'left' ):
+	$zenwares_left_span_class = 'col-lg-'.zenwares_options()->getCpanelValue('sidebar_left_expand');
+	$zenwares_left_span_class .= ' col-md-'.zenwares_options()->getCpanelValue('sidebar_left_expand_md');
+	$zenwares_left_span_class .= ' col-sm-'.zenwares_options()->getCpanelValue('sidebar_left_expand_sm');
+?>
+<aside id="left" class="sidebar <?php echo esc_attr($zenwares_left_span_class); ?>">
+	<?php dynamic_sidebar('left-product'); ?>
+</aside>
+
+<?php endif; ?>
+<div id="contents" <?php zenwares_content_product(); ?> role="main">
+	<?php
+		/**
+		 * woocommerce_before_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		 global $post;
+		do_action('woocommerce_before_main_content');
+	?>
+		<div class="products-wrapper">
+		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+		<?php endif; ?>
+
+		<?php if ( zenwares_sidebar_check('sidebar-top-cat') ):?>
+			<div class="sidebar-top-cat clearfix">
+				<?php dynamic_sidebar('sidebar-top-cat'); ?>
+			</div>
+		<?php endif; ?>
+
+		<?php if ( have_posts() ) : ?>
+		    <?php do_action('woocommerce_message'); ?>
+			<?php
+				/**
+				 * woocommerce_before_shop_loop hook
+				 *
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				do_action( 'woocommerce_before_shop_loop' );
+			?>
+			<div class="clear"></div>
+			<?php woocommerce_product_loop_start(); ?>
+
+				<?php woocommerce_product_subcategories(); ?>
+				<li class="clearfix divider-product"></li>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php wc_get_template_part( 'content', 'product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php woocommerce_product_loop_end(); ?>
+			<div class="clear"></div>
+			<?php
+				/**
+				 * woocommerce_after_shop_loop hook
+				 *
+				 * @hooked woocommerce_pagination - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop' );
+			?>
+		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+
+			<?php woocommerce_get_template( 'loop/no-products-found.php' ); ?>
+
+		<?php endif; ?>
+	</div>
+	<?php
+		/**
+		 * woocommerce_after_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action('woocommerce_after_main_content');
+	?>
+
+</div>
+<?php if ( zenwares_sidebar_check('right-product') && $zenwares_sidebar_product == 'right' ):
+	$zenwares_right_span_class = 'col-lg-'.zenwares_options()->getCpanelValue('sidebar_right_expand');
+	$zenwares_right_span_class .= ' col-md-'.zenwares_options()->getCpanelValue('sidebar_right_expand_md');
+	$zenwares_right_span_class .= ' col-sm-'.zenwares_options()->getCpanelValue('sidebar_right_expand_sm');
+?>
+<aside id="right" class="sidebar <?php echo esc_attr($zenwares_right_span_class); ?>">
+
+	<?php dynamic_sidebar('right-product'); ?>
+</aside>
+<?php endif; ?>
+
+</div>
+</div>
+</div>
+<?php get_template_part('footer'); ?>
